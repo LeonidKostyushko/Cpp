84a3f8d26a39a1e8f9e2bdfdd6d228231265cc62
LeonidKostyushko <48299706+LeonidKostyushko@users.noreply.github.com>
2019-04-22 16:51:06 +0300

    Add files via upload

--- hw4/serializer.h
+++ hw4/serializer.h
@@ -17,114 +17,114 @@ class Serializer
 	{
 	}
 
-template <class T>
-Error save(T& object)
-{
-	return object.serialize(*this);
-}
+	template <class T>
+	Error save(T& object)
+	{
+		return object.serialize(*this);
+	}
 
-template <class... ArgsT>
-Error operator()(ArgsT&&... args)
-{
-	return process(std::forward<ArgsT>(args)...);
-}
+	template <class... ArgsT>
+	Error operator()(ArgsT&&... args)
+	{
+		return process(std::forward<ArgsT>(args)...);
+	}
 
-private:
-template <class T, class... ArgsT>
-Error process(T&& value, ArgsT&&... args)
-{
-	save(std::forward<T>(value));
-	out << Separator;
+	private:
+	template <class T, class... ArgsT>
+	Error process(T&& value, ArgsT&&... args)
+	{
+		save(std::forward<T>(value));
+		out << Separator;
 
-	return process(std::forward<ArgsT>(args)...);
-}
+		return process(std::forward<ArgsT>(args)...);
+	}
 
-template <class T>
-Error process(T&& value)
-{
-	save(std::forward<T>(value));
-	return Error::NoError;
-}
+	template <class T>
+	Error process(T&& value)
+	{
+		save(std::forward<T>(value));
+		return Error::NoError;
+	}
 
-void save(bool value) 
-{
-	out << (value ? "true" : "false") << Separator;
-}
+	void save(bool value) 
+	{
+		out << (value ? "true" : "false") << Separator;
+	}
 
-void save(uint64_t value) 
-{
-	out << value << Separator;
-}
+	void save(uint64_t value) 
+	{
+		out << value << Separator;
+	}
 };
 
 class Deserializer
 {
-std::istream& in;
-static constexpr char Separator = ' ';
+	std::istream& in;
+	static constexpr char Separator = ' ';
 
-public:
-explicit Deserializer(std::istream& in_)
-	: in(in_) {}
+	public:
+	explicit Deserializer(std::istream& in_)
+		: in(in_) {}
 
-template <class T>
-Error load(T& obj) 
-{
-	return obj.serialize(*this);
-}
-
-template <class... ArgsT>
-Error operator()(ArgsT&&... args)
-{
-	return process(std::forward<ArgsT>(args)...);
-}
-
-private:
-template <class T,class...ArgsT>
-Error process(T&& value, ArgsT&&	...args)
-{
-	if (load(std::forward<T>(value)) == false) return Error::CorruptedArchive;
-
-	return process(std::forward<ArgsT>(args)...);
-}
-
-template <class T>
-Error process(T&& value)
-{
-	if (load(std::forward<T>(value)) == false)  return Error::CorruptedArchive;
-
-	return Error::NoError;
-}
-
-bool load(bool& value)
-{
-	std::string line;
-	in >> line;
-	if (line == "true")
+	template <class T>
+	Error load(T& obj) 
 	{
-		value = true;
-		return true;
-	}
-	else if (line == "false")
-	{
-		value = false;
-		return true;
+		return obj.serialize(*this);
 	}
 
-	return false;
-}
-bool load(uint64_t& value) 
-{
-	std::string line;
-	in >> line;
-	try 
+	template <class... ArgsT>
+	Error operator()(ArgsT&&... args)
 	{
-		if (line[0] == '-') return false;
-		value = std::stoull(line);
+		return process(std::forward<ArgsT>(args)...);
 	}
-	catch (std::invalid_argument)
+
+	private:
+	template <class T,class...ArgsT>
+	Error process(T&& value, ArgsT&&	...args)
 	{
+		if (load(std::forward<T>(value)) == false) return Error::CorruptedArchive;
+
+		return process(std::forward<ArgsT>(args)...);
+	}
+
+	template <class T>
+	Error process(T&& value)
+	{
+		if (load(std::forward<T>(value)) == false)  return Error::CorruptedArchive;
+
+		return Error::NoError;
+	}
+
+	bool load(bool& value)
+	{
+		std::string line;
+		in >> line;
+		if (line == "true")
+		{
+			value = true;
+			return true;
+		}
+		else if (line == "false")
+		{
+			value = false;
+			return true;
+		}
+
 		return false;
 	}
-	return true;
-}
+	bool load(uint64_t& value) 
+	{
+		std::string line;
+		in >> line;
+		try 
+		{
+			if (line[0] == '-') return false;
+			value = std::stoull(line);
+		}
+		catch (std::invalid_argument)
+		{
+			return false;
+		}
+		return true;
+	}
 };
