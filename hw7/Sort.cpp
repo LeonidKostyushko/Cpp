34f44e95bd3cc2ba624975f5f9b31db78086592a
LeonidKostyushko <48299706+LeonidKostyushko@users.noreply.github.com>
2019-06-19 22:06:00 +0300

    Add files via upload

--- hw7/Sort.cpp
+++ hw7/Sort.cpp
@@ -19,33 +19,40 @@ void conc(const string &f_1, const string &f_2, const string &conc)
 	ifstream f1(f_1, ios::binary);
 	ifstream f2(f_2, ios::binary);
 	ofstream res(conc, std::ios::binary);
-
-	f1.read(reinterpret_cast<char*>(&x), sizeof(x));
-	f2.read(reinterpret_cast<char*>(&y), sizeof(y));
-
-	while (!f1.eof() && !f2.eof())
+	if (!f1.is_open() || !f2.is_open() || !res.is_open())
 	{
-		if (x > y)
+		cout << "Файл не может быть открыт!" << endl;
+	}
+	else 
+	{
+
+		f1.read(reinterpret_cast<char*>(&x), sizeof(x));
+		f2.read(reinterpret_cast<char*>(&y), sizeof(y));
+
+		while (!f1.eof() && !f2.eof())
 		{
-			res.write(reinterpret_cast<char*>(&y), sizeof(y));
-			f2.read(reinterpret_cast<char*>(&y), sizeof(y));
+			if (x > y)
+			{
+				res.write(reinterpret_cast<char*>(&y), sizeof(y));
+				f2.read(reinterpret_cast<char*>(&y), sizeof(y));
+			}
+			else
+			{
+				res.write(reinterpret_cast<char*>(&x), sizeof(x));
+				f1.read(reinterpret_cast<char*>(&x), sizeof(x));
+			}
 		}
-		else
+		ifstream* fin = &f1;
+		if (f1.eof())
+		{
+			fin = &f2;
+			swap(x, y);
+		}
+		while (!(*fin).eof())
 		{
 			res.write(reinterpret_cast<char*>(&x), sizeof(x));
-			f1.read(reinterpret_cast<char*>(&x), sizeof(x));
+			(*fin).read(reinterpret_cast<char*>(&x), sizeof(x));
 		}
-	}
-	ifstream* fin = &f1;
-	if (f1.eof())
-	{
-		fin = &f2;
-		swap(x, y);
-	}
-	while (!(*fin).eof())
-	{
-		res.write(reinterpret_cast<char*>(&x), sizeof(x));
-		(*fin).read(reinterpret_cast<char*>(&x), sizeof(x));
 	}
 }
 
@@ -53,11 +60,18 @@ vector<uint64_t> read_numbers(ifstream &file, int amount)
 {
 	vector<uint64_t> arr;
 	uint64_t tmp;
-	for (int i = 0; i < amount; i++)
+	if (!file.is_open())
 	{
-		file.read(reinterpret_cast<char*>(&tmp), sizeof(tmp));
-		if (file.eof()) return arr;
-		arr.push_back(tmp);
+		cout << "Файл не может быть открыт!" << endl;
+	}
+	else
+	{
+		for (int i = 0; i < amount; i++)
+		{
+			file.read(reinterpret_cast<char*>(&tmp), sizeof(tmp));
+			if (file.eof()) return arr;
+			arr.push_back(tmp);
+		}
 	}
 	return arr;
 }
@@ -69,33 +83,38 @@ void p_sort(ifstream &file, mutex &mtx)
 
 	ostringstream ss;
 	ss << thread_id << "_";
-
-	while (!file.eof())
+	if (!file.is_open())
 	{
-		unique_lock<std::mutex> lock(mtx);
-
-		vector<uint64_t> arr = read_numbers(file, max_numbers);
-		if (arr.empty()) break;
-		lock.unlock();
-		sort(arr.begin(), arr.end());
-
-		ofstream out(ss.str() + to_string(++n), ios::binary);
-		out.write(reinterpret_cast<char*>(arr.data()), arr.size() * sizeof(uint64_t));
-		this_thread::yield();
+		cout << "Файл не может быть открыт!" << endl;
 	}
+	else {
+		while (!file.eof())
+		{
+			unique_lock<std::mutex> lock(mtx);
 
-	while (n > 1)
-	{
-		unique_lock<mutex> lock(mtx);
-		string f1 = ss.str() + to_string(n--);
-		string f2 = ss.str() + to_string(n);
-		string tmp = ss.str() + string("tmp");
-		conc(ref(f1), ref(f2), ref(tmp));
-		lock.unlock();
-		remove(f1.c_str());
-		remove(f2.c_str());
-		rename(tmp.c_str(), f2.c_str());
-		this_thread::yield();
+			vector<uint64_t> arr = read_numbers(file, max_numbers);
+			if (arr.empty()) break;
+			lock.unlock();
+			sort(arr.begin(), arr.end());
+
+			ofstream out(ss.str() + to_string(++n), ios::binary);
+			out.write(reinterpret_cast<char*>(arr.data()), arr.size() * sizeof(uint64_t));
+			this_thread::yield();
+		}
+
+		while (n > 1)
+		{
+			unique_lock<mutex> lock(mtx);
+			string f1 = ss.str() + to_string(n--);
+			string f2 = ss.str() + to_string(n);
+			string tmp = ss.str() + string("tmp");
+			conc(ref(f1), ref(f2), ref(tmp));
+			lock.unlock();
+			remove(f1.c_str());
+			remove(f2.c_str());
+			rename(tmp.c_str(), f2.c_str());
+			this_thread::yield();
+		}
 	}
 }
 
@@ -119,6 +138,11 @@ int main(int argc, char **argv)
 	else return -1;
 
 	ifstream file(filename, ios::binary);
+	if (!file.is_open())
+	{
+		cout << "Файл не может быть открыт!" << endl;
+		return 0;
+	}
 	mutex mtx;
 	thread t1(p_sort, ref(file), ref(mtx));
 	thread t2(p_sort, ref(file), ref(mtx));
